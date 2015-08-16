1b08f18fed1cc65813d69ceb8df7f66c015301bd
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-16 16:12:40 -0500

    dashboard route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get    '/auth/github/callback', to: 'sessions#create'
   delete 'logout',                to: 'sessions#destroy'
   get    '/auth/github',          as: :login
+  get    '/dashboard',            to: 'dashboard#show'
   root   'home#show'
 end
 
