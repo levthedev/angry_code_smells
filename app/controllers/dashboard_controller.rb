fcfec03b93b1cdd795f64285ddd84f6b0f37aaf7
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-16 16:03:35 -0500

    dashboard controller

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,9 @@
+class DashboardController < ApplicationController
+  def show
+    @feed = client.user_events(current_user.nickname)
+  end
+
+  def client
+    @client ||= Octokit::Client.new(access_token: current_user.token)
+  end
+end
