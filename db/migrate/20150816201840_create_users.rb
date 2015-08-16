4fc6a9311e560f4448485016db63819f11cc41f2
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-16 15:19:01 -0500

    add user model

---
+++ db/migrate/20150816201840_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :nickname
+      t.string :email
+      t.string :provider
+      t.string :token
+      t.string :uid
+      t.string :image_url
+      t.string :twitter
+
+      t.timestamps null: false
+    end
+  end
+end
