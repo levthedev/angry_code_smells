c16db12b0f9127724596eae7f109986f24a38e1d
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-16 16:30:21 -0500

    change dashboard to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  def show
+    @user = current_user
+  end
+
+  def update
+    if current_user.update_attributes(twitter: params[:user][:twitter])
+      redirect_to user_path(current_user)
+    end
+  end
 end
