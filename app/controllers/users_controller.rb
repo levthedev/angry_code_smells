7f2e2ebeb60aa8ceb2f43e9db5d9ae92db412ebe
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-20 19:58:49 -0700

    experimenting with PRs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,24 +8,4 @@ class UsersController < ApplicationController
       redirect_to user_path(current_user)
     end
   end
-
-  private
-
-  def client
-    @client ||= Octokit::Client.new(access_token: current_user.token)
-  end
-
-  def repos
-    @client.repos.map(&:name)
-  end
-
-  def pull_requests(repo)
-    @client.pull_requests("/#{current_user.nickname}/#{repo}")
-  end
-
-  def pull_request_comments(repo)
-    pull_requests(repo).each_with_index do |pull, index|
-      @client.pull_request_comments("#{current_user.nickname}/#{pull}", index + 1).map(&:body)
-    end
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,28 @@ class User < ActiveRecord::Base
     user.save
     user
   end
+
+  def client
+    client = Octokit::Client.new(access_token: token)
+    Octokit.auto_paginate = true
+    client
+  end
+
+  def repos
+    Octokit.auto_paginate = true
+    client.repos.map(&:name)
+  end
+
+  def pull_requests(repo)
+    Octokit.auto_paginate = true
+    client.pull_requests("#{nickname}/#{repo}")
+  end
+
+  def pull_request_comments(repo)
+    Octokit.auto_paginate = true
+    pull_requests(repo).each_with_index do |pull, index|
+      pr_comments = client.pull_request_comments("#{nickname}/#{pull}", index + 1)
+      pr_comments.map(&:body)
+    end
+  end
 end
